9d8410bf1c760b1ea933929a37f3e12773fea8f4
Prayas-dot <anshchoukikar3@gmail.com>
2024-08-14 01:53:30 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14456
+let accountEmail = "anshc@gmail.com"
+var accountPassword ="19990"
+accountCity = "bhopal"
+ //accountId = 1
+ //console.log(accountId)
+ accountEmail = "pr@c.com"
+ accountPassword = "000"
+ accountCity = "indore"
+
+ console.table([accountId , accountEmail , accountPassword , accountCity])
