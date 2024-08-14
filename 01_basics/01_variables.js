98ad16d7b32797fa57c9dd871cdf8444b4062d41
Prayas-dot <anshchoukikar3@gmail.com>
2024-08-14 02:49:35 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,7 @@ var accountPassword ="19990"
 accountCity = "bhopal"
  //accountId = 1
  //console.log(accountId)
+ // prefer not to use var because of issue in block scope and functional scope 
  accountEmail = "pr@c.com"
  accountPassword = "000"
  accountCity = "indore"
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+//alert(3+3)// we are using node js not browser
+// code readibility should be high rg:-
+console.log(3
+    
+
+);
+let name = "Anshul"// string dataType
+let age = 18 //num dataType
+let isloggedin = false // boolean
+
+//number = 2 to power 53 
+//bigint datatype 
+// string datatype prefer ""
+// boolean datatype 
+//null is standalone value
+//undefined 
+//symbol
+
+
+//object
+
+console.log(typeof undefined);
