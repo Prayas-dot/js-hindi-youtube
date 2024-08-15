ef28c85274eace6437ce0485bdf29826b2ce9fe8
Prayas-dot <anshchoukikar3@gmail.com>
2024-08-15 08:05:18 +0000

    type of operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,6 +11,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//*****************************************operations*******************************//
+
+let value = 3
+let negativeValue = -value
+//console.log(negativeValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);//power
+//console.log(2/2);//divide
+//console.log(2%2);//remainder
+
+let str1 = "hello"
+let str2 = " anshul"
+
+let str3 = str1 +str2 
+
+// agar string se start hota h toh output string me ayega 
+// agar number me satrt hota h toh output number me ayega
+// paranthesis use kana imp h
+
+//console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
 
