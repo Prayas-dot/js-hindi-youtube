e36fd7f27e3457dd603fc011b79ea60e393584dc
Prayas-dot <anshchoukikar3@gmail.com>
2024-08-15 07:31:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let isLoggedIn ="anshul"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 = true 
+//0 = false
+// empty strings = false 
+// string = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
