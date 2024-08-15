b11bf4f1e56038e97f613f3a5a3cc8dc14b52e94
Prayas-dot <anshchoukikar3@gmail.com>
2024-08-15 09:03:40 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+// tricky conversions bcz it dont compare datatype 
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);// in this case it converts null into 
+ 
+//comparisions on undefined datatype are also tricky we usually avoid that
+
+
+//"===" its used for strict comparisions it also compares datatype
+
+
